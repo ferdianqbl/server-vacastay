c4fd678bc6687e2180aad05cc97a2ed7a28de9bf
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-01-18 23:44:38 +0700

    add category ejs and contain table

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,12 +1,14 @@
 const viewDashboard = (req, res) => {
   res.render("admin/dashboard/view_dashboard", {
     title: "Vacastay | Dashboard",
+    type: "dashboard",
   });
 };
 
 const viewCategory = (req, res) => {
   res.render("admin/category/view_category", {
     title: "Vacastay | Category",
+    type: "category",
   });
 };
 
