53a7631d4f133f18dd7e7deb6ccc229871424b4a
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-01-22 14:39:25 +0700

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -386,9 +386,6 @@ const deleteItem = async (req, res) => {
     }
 
     // delete itemId in category collection
-    const oldCategory = await Category.findOne({
-      _id: item.categoryId.id,
-    });
 
     // const filteredItemId = oldCategory.itemId.filter((result) => {
     //   return result.toString() !== item._id.toString();
