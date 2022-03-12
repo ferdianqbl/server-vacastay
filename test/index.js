61f3ecd88a7be5db33af3f86552b85c7f496ba0e
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-03-12 20:18:54 +0700

    add new testing

--- test/index.js
+++ test/index.js
@@ -2,11 +2,12 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const expect = chai.expect;
 const app = require("../app");
+const fs = require("fs");
 
 chai.use(chaiHttp);
 
 describe("API ENDPOINT TESTING", () => {
-  it("GET LANDING PAGE", (done) => {
+  it("GET Landing Page", (done) => {
     chai
       .request(app)
       .get("/api/v1/member/landing-page")
@@ -30,7 +31,7 @@ describe("API ENDPOINT TESTING", () => {
       });
   });
 
-  it("GET DETAIL PAGE", (done) => {
+  it("GET Detail Page", (done) => {
     chai
       .request(app)
       .get("/api/v1/member/detail-page/62143218efe86cde28e7e10a")
@@ -63,6 +64,72 @@ describe("API ENDPOINT TESTING", () => {
 
         expect(res.body).to.have.property("testimonial");
         expect(res.body.testimonial).to.have.an("object");
+        done();
+      });
+  });
+
+  it("POST Booking Page", (done) => {
+    const image = __dirname + "/buktibayar.jpeg";
+    const dataSample = {
+      image,
+      duration: "2",
+      bookingStartDate: "09-04-2022",
+      bookingEndDate: "12-04-2022",
+      firstName: "adrian",
+      lastName: "firdaus",
+      emailAddress: "adrian@gmail.com",
+      phoneNumber: "0812212133113",
+      accountHolder: "adrian",
+      bankFrom: "BCA",
+      itemId: "62143218efe86cde28e7e10a",
+    };
+    chai
+      .request(app)
+      .post("/api/v1/member/booking-page")
+      .set("Content-Type", "application/x-www-form-urlencoded")
+      .field("itemId", dataSample.itemId)
+      .field("duration", dataSample.duration)
+      .field("bookingStartDate", dataSample.bookingStartDate)
+      .field("bookingEndDate", dataSample.bookingEndDate)
+      .field("firstName", dataSample.firstName)
+      .field("lastName", dataSample.lastName)
+      .field("emailAddress", dataSample.emailAddress)
+      .field("phoneNumber", dataSample.phoneNumber)
+      .field("accountHolder", dataSample.accountHolder)
+      .field("bankFrom", dataSample.bankFrom)
+      .attach("image", fs.readFileSync(dataSample.image), "buktibayar.jpeg")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(201);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("message");
+        expect(res.body.message).to.equal("Successfully booked");
+
+        expect(res.body).to.have.property("booking");
+        expect(res.body.booking).to.have.all.keys(
+          "payment",
+          "_id",
+          "invoice",
+          "bookingStartDate",
+          "bookingEndDate",
+          "total",
+          "itemId",
+          "memberId",
+          "__v"
+        );
+        expect(res.body.booking.payment).to.have.all.keys(
+          "status",
+          "proofPayment",
+          "bankFrom",
+          "accountHolder"
+        );
+        expect(res.body.booking.itemId).to.have.all.keys(
+          "_id",
+          "title",
+          "price",
+          "duration"
+        );
+        console.log(res.body.booking);
         done();
       });
   });
