a6c18cb13d6b7ab3de3faf629209156844e5d3ba
ferdianqbl <muhammadferdianiqbal@gmail.com>
2022-03-12 19:15:42 +0700

    add new testing

--- test/index.js
+++ test/index.js
@@ -13,7 +13,7 @@ describe("API ENDPOINT TESTING", () => {
       .end((err, res) => {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
-        expect(res.body).to.be.an("Object");
+        expect(res.body).to.be.an("object");
         expect(res.body).to.have.property("hero");
         expect(res.body.hero).to.have.all.keys(
           "travelers",
@@ -24,6 +24,43 @@ describe("API ENDPOINT TESTING", () => {
         expect(res.body.mostPicked).to.have.an("array");
         expect(res.body).to.have.property("categories");
         expect(res.body.categories).to.have.an("array");
+        expect(res.body).to.have.property("testimonial");
+        expect(res.body.testimonial).to.have.an("object");
+        done();
+      });
+  });
+
+  it("GET DETAIL PAGE", (done) => {
+    chai
+      .request(app)
+      .get("/api/v1/member/detail-page/62143218efe86cde28e7e10a")
+      .end((err, res) => {
+        expect(err).to.be.null;
+        expect(res).to.have.status(200);
+        expect(res.body).to.be.an("object");
+        expect(res.body).to.have.property("country");
+        expect(res.body).to.have.property("isPopular");
+        expect(res.body).to.have.property("sumBooking");
+
+        expect(res.body).to.have.property("imageId");
+        expect(res.body.imageId).to.have.an("array");
+
+        expect(res.body).to.have.property("featureId");
+        expect(res.body.featureId).to.have.an("array");
+
+        expect(res.body).to.have.property("activityId");
+        expect(res.body.activityId).to.have.an("array");
+
+        expect(res.body).to.have.property("_id");
+        expect(res.body).to.have.property("title");
+        expect(res.body).to.have.property("price");
+        expect(res.body).to.have.property("city");
+        expect(res.body).to.have.property("description");
+        expect(res.body).to.have.property("__v");
+
+        expect(res.body).to.have.property("banks");
+        expect(res.body.banks).to.have.an("array");
+
         expect(res.body).to.have.property("testimonial");
         expect(res.body.testimonial).to.have.an("object");
         done();
